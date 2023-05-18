ab688ec1f20310bccc6ac21dfa37fe8ceb851f38
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-05-18 00:36:56 -0700

    commit 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(){
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a sentence using puts function
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
