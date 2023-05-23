047445a7047cf2f1643e0d156da3f8267fa4ace1
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-05-23 14:15:57 -0700

    commit 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+
+/**
+ * _abs - A function that computes the absolute value of an integer.
+ * @n: number to compute its absolute value.
+ *
+ * Return: n if number is positive
+ * otherwise -n
+*/
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
