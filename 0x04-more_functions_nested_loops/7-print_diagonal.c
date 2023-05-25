e0bb4a9aba18722c8aabc45bc7e2a07443e30b1f
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-05-25 03:18:54 -0700

    commit 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,9 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
 		for (i = 1; i <= n; i++)
 		{
@@ -17,9 +19,5 @@ void print_diagonal(int n)
 			_putchar(92);
 			_putchar('\n');
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
