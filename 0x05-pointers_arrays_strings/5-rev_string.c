5507cf95c2ce3412981cd04993517fd994c1b25f
khadijarejjaoui99 <khadijarejjaoui41@gmail.com>
2023-05-29 03:52:45 -0700

    commit 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,17 +1,28 @@
-
+#include "main.h"
 
 /**
  * rev_string - reverses a string
  * @s: string to be reversed
-*/
+ */
 void rev_string(char *s)
 {
-	int i, temp;
-	int len = sizeof(s) / sizeof(s[0]);
-	for (i = 0; i < len; i++)
+	char tmp;
+	int i, len, len1;
+
+	len = 0;
+	len1 = 0;
+
+	while (s[len] != '\0')
 	{
-		temp = s[i];
-		s[i] = s[len];
-		s[len--] = temp;
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
